5c3fe84c12989d59cc132d34eb20fe98fd1bc5d2
аывпфывпирывар <sxfgbvszdfb>
2023-04-12 21:31:27 +0300

    hw5 probably

--- programm1.java
+++ programm1.java
@@ -1,40 +1,21 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class programm1 {
-    public static void main(String[] args) {   
-    Deque<Integer> Mydeque = createArray();
-    
-    if (checkOn(Mydeque)==true) {
-        System.out.println("палиндром");
-    }
-    else{
-        System.out.println("не палиндром");
+public static void main(String[] args) {   
+    Scanner sc = new Scanner(System.in);
+    HashMap<String, String> phones = new HashMap<>();
+    System.out.println("кол-во записей"); 
+    int count = sc.nextInt();
+    System.out.println("ФИО:номер телефона"); 
+    for (int i = 0; i < count; i++) {
+        String[] str = sc.next().split(":");
+        if (phones.containsKey(str[0])) {
+            phones.put(str[0], phones.get(str[0]) + "," + str[1]);
+        } else {
+            phones.put(str[0],str[1]);
         }
     }
-    public static Deque<Integer> createArray(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("кол-во чисел");
-        int x = scan.nextInt();
-        Deque<Integer> lst = new ArrayDeque<>(x);
-        for (int j = 0; j < x; j++) {
-            int number = scan.nextInt();
-            lst.add(number);
-        }
-        return lst;
-        }
-    public static boolean checkOn(Deque<Integer> deque){
-        boolean res = true;
-        for (int i = 0; i < deque.size() / 2; i ++) {
-            int first = deque.pollFirst();
-            int last = deque.pollLast();
-                if (first!=last) {
-                    res = false;
-                    break;
-                }
-            }
-    return res;
+    System.out.print(phones);
     }
-
 }
--- programm2.java
+++ programm2.java
@@ -1,65 +1,33 @@
-import java.util.ArrayDeque;
-// import java.util.Arrays;
-import java.util.Deque;
-// import java.util.LinkedList;
-import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.TreeMap;
 public class programm2 {
-
-//Даны два Deque представляющие два неотрицательных целых числа. Цифры хранятся в обратном порядке,
-// и каждый из их узлов содержит одну цифру.
-// Сложите два числа и верните сумму в виде связанного списка.
-public static void main(String[] args) {
-    Deque<Integer> d1 = createArray();
-    Deque<Integer> d2 = createArray();
-    // Deque<Integer> Llist = new ArrayDeque<Integer>();
-    // String str1 = new String();
-    // String str2 = new String();
-    // int length1 = deque1.size();
-    // int length2 = deque2.size();
-    // for (int index = 0; index < length1; index++) {
-    // str1 += deque1.pollLast();
-    // }
-    // for (int i = 0; i < length2; i++) {
-    // str2 += deque2.pollLast();
-    // }
-    // int summ = Integer.parseInt(str1) + Integer.parseInt(str2);
-    // while (summ > 0 ) {
-    // Llist.add(summ%10);
-    // summ = summ/10;
-    // }
-    System.out.println(sum(d1,d2));
-    }
-    
-    public static Deque<Integer> createArray(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("кол-во чисел");
-        int x = scan.nextInt();
-        Deque<Integer> lst = new ArrayDeque<>(x);
-        for (int j = 0; j < x; j++) {
-            int number = scan.nextInt();
-            lst.add(number);
+    public static void main(String[] args) {   
+        String[] names = {"Иван Иванов", "Светлана Петрова", "Кристина Белова", "Анна Мусина", "Анна Крутова","Иван Юрин","Петр Лыков","Павел Чернов","Петр Чернышов","Мария Федорова","Марина Светлова","Мария Савина","Мария Рыкова","Марина Лугова","Анна Владимирова","Иван Мечников","Петр Петин","Иван Ежов"};
+        TreeMap<String, String> Hashnames = new TreeMap<>(); 
+        for (String exp : names) {
+            String [] temp = exp.split(" ");
+            if (Hashnames.containsKey(temp[1])) {
+                Hashnames.put(temp[1], Hashnames.get(temp[1]) + "," + temp[0]);
+            } else {
+                Hashnames.put(temp[1],temp[0]);
+            }
         }
-        return lst;
-        }
+        ArrayList<String> hachName = new ArrayList<>(Hashnames.values());
+        TreeMap<String, Integer> res = new TreeMap<>();
 
-public static Deque<Integer> sum(Deque<Integer> deque1, Deque<Integer> deque2) {
-    Deque<Integer> Llist = new ArrayDeque<Integer>();
-    String str1 = new String();
-    String str2 = new String();
-    int length1 = deque1.size();
-    int length2 = deque2.size();
-    for (int index = 0; index < length1; index++) {
-    str1 += deque1.pollLast();
-    }
-    for (int i = 0; i < length2; i++) {
-    str2 += deque2.pollLast();
-    }
-    int summ = Integer.parseInt(str1) + Integer.parseInt(str2);
-    while (summ > 0 ) {
-    Llist.add(summ%10);
-    summ = summ/10;
-    }
-    return Llist;
+        for (String j : hachName) {
+            if (res.containsKey(j)) {
+                res.put(j, res.get(j)+1);
+            } else {
+                res.put(j,1);
+            }
+        }
+        ArrayList<String> sorNames = new ArrayList<>(res.keySet());
+        for (String i : sorNames) {
+            if(res.get(i)==1){
+                res.remove(i);
+            }
+        }
+        System.out.print(res);
 }
 }
